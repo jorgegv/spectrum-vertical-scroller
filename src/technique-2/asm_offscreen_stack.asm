ea143c1d74f9f5b4da235acc50538a5fefdf95aa
ZXjogv <zx@jogv.es>
2023-01-09 23:29:34 +0100

    technique-2: reorganized asm labels

--- src/technique-2/asm_offscreen_stack.asm
+++ src/technique-2/asm_offscreen_stack.asm
@@ -23,7 +23,7 @@ _asm_offscreen_show_frame_stack:
 	push iy
 
 	;; use SMC to save SP to the position at the end of this routine
-	ld (restore_sp - 2),sp
+	ld (restore_sp + 1),sp
 
 	;; BC = offscreen_line_address[ current_scroll_start_line ]
 	ld a,(_current_scroll_start_line)
@@ -35,7 +35,7 @@ _asm_offscreen_show_frame_stack:
 	ld c,(hl)
 	inc hl
 	ld b,(hl)
-	ld (switch_sp_2 - 2),bc	;; save initial address of first src line
+	ld (switch_sp_src + 1),bc	;; save initial address of first src line
 
 	;; set A (line loop counter) to number of lines to draw
 	ld a,SCROLL_LINES
@@ -43,13 +43,13 @@ _asm_offscreen_show_frame_stack:
 	;; set initial ptr to first dst line address
 	;; right to left!
 	ld hl,_screen_line_end_address
-	ld (switch_sp_1 - 2),hl
+	ld (switch_sp_dst + 2),hl
 
 loop1:
 	;; src and dst addresses are ready
 	;; read 16 bytes from src into regs
+switch_sp_src:
 	ld sp,$ffff		;; SMC: $ffff is used as a variable and modified at the top
-switch_sp_2:
 
 	pop ix
 	pop bc
@@ -62,11 +62,11 @@ switch_sp_2:
 	pop hl
 
 	;; save current src for next line
-	ld (switch_sp_2 - 2),sp
+	ld (switch_sp_src + 1),sp
 
+switch_sp_dst:
 	ld sp,($ffff)	;; SMC - $ffff value will be the temporary storage
 			;; for ptr to dst address
-switch_sp_1:
 
 	push hl
 	push de
@@ -82,30 +82,30 @@ switch_sp_1:
 	;; beginning of the offscreen:
 	;; DE = addr for next line (we saved it before)
 	;; HL = addr of end of offscreen + 1
-	ld de,(switch_sp_2 - 2)
+	ld de,(switch_sp_src + 1)
 	ld hl,_offscreen + ( ( SCROLL_LINES + SCROLL_EXTRA_LINES ) * SCROLL_COLS )
 	sbc hl,de		;; check if DE >= HL
 	jr z,reset_1		;; reset src if equal...
 	jr nc,skip_reset_1	;; skip reset if HL is greater
 reset_1:
 	ld hl,_offscreen	;; ...or less
-	ld (switch_sp_2 - 2),hl
+	ld (switch_sp_src + 1),hl
 skip_reset_1:
 
 	;; adjust dst address ptr for next iteration: load next address from
 	;; LUT
-	ld hl,(switch_sp_1 - 2)
+	ld hl,(switch_sp_dst + 2)
 	inc hl
 	inc hl
-	ld (switch_sp_1 - 2),hl
+	ld (switch_sp_dst + 2),hl
 
 	;; dec counter and check
 	dec a
 	jr nz, loop1
 
 	;; restore SP
-	ld sp,$ffff		;; SMC: $ffff is used as a variable and modified at the top
 restore_sp:
+	ld sp,$ffff		;; SMC: $ffff is used as a variable and modified at the top
 
 	;; restore IY and reenable ints
 	pop iy
